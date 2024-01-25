699c3b6b210583e451176e4166eefab418a7e64f
ahgddev <ahgddev@gmail.com>
2024-01-25 15:54:03 -0700

    Added menuLinks variable

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 import "./styles.css";
 
+var menuLinks = [
+    {text: 'about', href: '/about'},
+    {text: 'catalog', href: '#', subLinks: [
+      {text: 'all', href: '/catalog/all'},
+      {text: 'top selling', href: '/catalog/top'},
+      {text: 'search', href: '/catalog/search'},
+    ]},
+    {text: 'orders', href: '#' , subLinks: [
+      {text: 'new', href: '/orders/new'},
+      {text: 'pending', href: '/orders/pending'},
+      {text: 'history', href: '/orders/history'},
+    ]},
+    {text: 'account', href: '#', subLinks: [
+      {text: 'profile', href: '/account/profile'},
+      {text: 'sign out', href: '/account/signout'},
+    ]},
+  ];
+  
 let subMenuEl = document.getElementById("sub-menu")
 subMenuEl.style.height = "100%";
 subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
