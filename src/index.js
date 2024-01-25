355d2d5e276e69ccd6d0f99b4a786a11b35304f2
ahgddev <ahgddev@gmail.com>
2024-01-25 16:28:12 -0700

    Cleaning up some bugs/issues.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
-import "./styles.css";
+
+let mainEl = document.getElementsByTagName("main")
+mainEl[0].style.backgroundColor = "var(--main-bg)"
+mainEl[0].innerHTML = "<h1>DOM Manipulation</h1>"
+mainEl[0].classList.add("flex-ctr");
+
+let topMenuEl = document.getElementById("top-menu")
+topMenuEl.style.height = "100%";
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+topMenuEl.classList.add("flex-around")
 
 var menuLinks = [
     {text: 'about', href: '/about'},
@@ -17,10 +26,21 @@ var menuLinks = [
       {text: 'sign out', href: '/account/signout'},
     ]},
   ];
-  
-let subMenuEl = document.getElementById("sub-menu")
-subMenuEl.style.height = "100%";
-subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
-subMenuEl.classList.add("flex-around");
-subMenuEl.style.position = "absolute";
-subMenuEl.style.top = "0";
+
+  //Create Nav
+for (link of menuLinks){
+    let newLink = topMenuEl.appendChild(document.createElement("a"))
+    newLink.href = link.href
+    newLink.innerText = link.text
+    console.log(link.href, link.text)
+ }
+ 
+ let subMenuEl = document.getElementById("sub-menu")
+ subMenuEl.style.height = "100%";
+ subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
+ subMenuEl.classList.add("flex-around");
+ subMenuEl.style.position = "absolute";
+ subMenuEl.style.top = "0";
+ 
+
+let topMenuLinks = topMenuEl.getElementsByTagName("a")
