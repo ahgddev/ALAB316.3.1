94ea7d9546d789342c2e6518ebdf6cbc693f23b1
ahgddev <ahgddev@gmail.com>
2024-01-25 15:53:34 -0700

    Started on Lab.

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./styles.css";
+
+let subMenuEl = document.getElementById("sub-menu")
+subMenuEl.style.height = "100%";
+subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
+subMenuEl.classList.add("flex-around");
+subMenuEl.style.position = "absolute";
+subMenuEl.style.top = "0";
