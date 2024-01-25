26e924d8270a3cf8af18e5370edcf714969669ba
ahgddev <ahgddev@gmail.com>
2024-01-25 16:36:04 -0700

    Working on onClick function.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
-
 let mainEl = document.getElementsByTagName("main")
 mainEl[0].style.backgroundColor = "var(--main-bg)"
 mainEl[0].innerHTML = "<h1>DOM Manipulation</h1>"
 mainEl[0].classList.add("flex-ctr");
-
-let topMenuEl = document.getElementById("top-menu")
-topMenuEl.style.height = "100%";
-topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
-topMenuEl.classList.add("flex-around")
 
 var menuLinks = [
     {text: 'about', href: '/about'},
@@ -27,20 +21,32 @@ var menuLinks = [
     ]},
   ];
 
-  //Create Nav
+
+ let topMenuEl = document.getElementById("top-menu")
+    topMenuEl.style.height = "100%";
+    topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+    topMenuEl.classList.add("flex-around")
+
+      //Create Nav
 for (link of menuLinks){
     let newLink = topMenuEl.appendChild(document.createElement("a"))
     newLink.href = link.href
     newLink.innerText = link.text
     console.log(link.href, link.text)
  }
- 
+
  let subMenuEl = document.getElementById("sub-menu")
- subMenuEl.style.height = "100%";
- subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
- subMenuEl.classList.add("flex-around");
- subMenuEl.style.position = "absolute";
- subMenuEl.style.top = "0";
+    subMenuEl.style.height = "100%";
+    subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
+    subMenuEl.classList.add("flex-around");
+    subMenuEl.style.position = "absolute";
+    subMenuEl.style.top = "0";
  
 
 let topMenuLinks = topMenuEl.getElementsByTagName("a")
+topMenuEl.addEventListener("click", function(event){
+    event.preventDefault()
+    if(!topMenuEl.querySelector("nav a:active")){
+        return console.log(topMenuEl.querySelector("nav a:active"))
+    }
+  });
